46102ea45231add115d46dbcf8fc02af248d4be1
Ranjan Kumar <mrranjankumarbxr@gmail.com>
2025-01-25 00:45:13 +0530

    Processing

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,29 @@
+// import { useEffect, useState } from "react"
+import { useLoaderData } from "react-router-dom"
+function Github() {
+    const data = useLoaderData()
+
+    // const [data,setData] = useState([])
+    // useEffect(()=>{
+    //     fetch("https://api.github.com/users/RanjanKumarBharti")
+    //     .then(response=> response.json())
+    //     .then(data=>{
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // })
+  return (
+    <div className=" bg-gray-600 text-3xl text-center p-4 m-4 text-white rounded-xl">
+      <div className=" pb-4">Github followers: {data.followers}</div>
+      <img src={data.avatar_url} alt="" />
+    </div>
+  )
+}
+
+export default Github
+
+
+export const githubInfoLoader = async () =>{
+    const response = await fetch('https://api.github.com/users/RanjanKumarBharti')
+    return response.json()
+} 
